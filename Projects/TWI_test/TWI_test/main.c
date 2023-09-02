fe5864a93335e857cf5cbd0101e22515f9df2c53
Ahmedalaalotfy <63472034+Ahmedalaalotfy@users.noreply.github.com>
2023-09-03 02:40:38 +0300

    Add files via upload

---
+++ Projects/TWI_test/TWI_test/main.c
@@ -0,0 +1,52 @@
+/*
+ * TWI_test.c
+ *
+ * Created: 10/6/2022 1:23:15 PM
+ * Author : ahmed
+ */ 
+
+#define F_CPU 16000000UL
+#include <util/delay.h>
+
+/* UTILES_LIB */
+#include   "STD_TYPES.h"
+#include   "BIT_MATH.h"
+#include   "ATMEGA32_REG.h"
+
+/* MCAL */
+#include "TWI_interface.h"
+#include "DIO_interface.h"
+
+/* HAL */
+#include "EEPROM_interface.h"
+#include "LCD_interface.h"
+#include "LCD_config.h"
+
+
+
+int main(void)
+{
+	 DIO_setPinDirection(DIO_PORTA,DIO_PIN4,DIO_PIN_OUTPUT);
+	 DIO_setPinDirection(DIO_PORTA,DIO_PIN5,DIO_PIN_OUTPUT);
+	 DIO_setPinDirection(DIO_PORTA,DIO_PIN6,DIO_PIN_OUTPUT);
+	 DIO_setPinDirection(DIO_PORTA,DIO_PIN7,DIO_PIN_OUTPUT);
+	 
+	 DIO_setPinDirection(DIO_PORTB,DIO_PIN1,DIO_PIN_OUTPUT);
+	 DIO_setPinDirection(DIO_PORTB,DIO_PIN2,DIO_PIN_OUTPUT);
+	 DIO_setPinDirection(DIO_PORTB,DIO_PIN3,DIO_PIN_OUTPUT);
+	 
+	 LCD_init();
+	
+	u8 data ;  
+	
+    TWI_initMaster(); 
+	
+	EEPROM_writeByte(94,'A');
+	EEPROM_readByte(94,&data); 
+	
+	LCD_sendChar(data);
+    while (1) 
+    {
+    }
+}
+
---
+++ Projects/UART_test/UART_test/main.c
@@ -0,0 +1,36 @@
+/*
+ * UART_test.c
+ *
+ * Created: 10/4/2022 9:29:10 AM
+ * Author : ahmed
+ */ 
+
+#define F_CPU 16000000UL
+#include <util/delay.h>
+
+/* UTILES_LIB */
+#include   "STD_TYPES.h"
+#include   "BIT_MATH.h"
+#include   "ATMEGA32_REG.h"
+
+/* MCAL */
+#include   "UART_interface.h"
+#include   "DIO_interface.h"
+
+
+int main(void)
+{
+	DIO_setPinDirection(DIO_PORTD,DIO_PIN0,DIO_PIN_INPUT); // Rx
+	DIO_setPinDirection(DIO_PORTD,DIO_PIN1,DIO_PIN_OUTPUT); //Tx
+	
+	UART_init(); 
+	
+    
+    while (1) 
+    {
+		UART_TxString((u8)"AHMED");
+		_delay_ms(1000);
+		
+    }
+}
+
